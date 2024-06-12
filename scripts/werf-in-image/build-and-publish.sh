a1a417d406003fc40a527b4063c0d8d4aaaa8fbb
Shajith-Clappit <skafridi@clappit.io>
2024-06-12 20:14:03 +0530

    build

--- scripts/werf-in-image/build-and-publish.sh
+++ scripts/werf-in-image/build-and-publish.sh
@@ -23,22 +23,16 @@ export WERF_EXPORT_ADD_LABEL_AH1=io.artifacthub.package.readme-url=https://raw.g
        WERF_EXPORT_ADD_LABEL_OC3=org.opencontainers.image.created=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
        WERF_EXPORT_ADD_LABEL_OC4=org.opencontainers.image.description="Official image to run werf in containers"
 
-werf export --tag "$DEST_SUBREPO/werf:latest" "1.2-stable-alpine"
-werf export --tag "$DEST_SUBREPO/werf-argocd-cmp-sidecar:latest" "argocd-cmp-sidecar-1.2-stable-ubuntu"
-
 for group in "1.2"; do
-  werf export --tag "$DEST_SUBREPO/werf:$group" "$group-stable-alpine"
-  werf export --tag "$DEST_SUBREPO/werf-argocd-cmp-sidecar:$group" "argocd-cmp-sidecar-$group-stable-ubuntu"
 
-  for distro in "alpine" "ubuntu" "ubuntu20" "centos" "fedora"; do
+  for distro in "ubuntu"; do
     werf export --tag "$DEST_SUBREPO/werf:$group-$distro" "$group-stable-$distro"
   done
 
-  for channel in "alpha" "beta" "ea" "stable" "rock-solid"; do
+  for channel in "stable"; do
     werf export --tag "$DEST_SUBREPO/werf:$group-$channel" "$group-$channel-alpine"
-    werf export --tag "$DEST_SUBREPO/werf-argocd-cmp-sidecar:$group-$channel" "argocd-cmp-sidecar-$group-$channel-ubuntu"
 
-    for distro in "alpine" "ubuntu" "ubuntu20" "centos" "fedora"; do
+    for distro in "ubuntu"; do
       werf export --tag "$DEST_SUBREPO/werf:$group-$channel-$distro" "$group-$channel-$distro"
     done
 
